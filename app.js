28d599157f83312bc7c0bce569d39b227393fd36
Tomolo997 <tomazovsenjak7@gmail.com>
2020-12-11 13:59:14 +0100

    added opacity and radius of the brush

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-let map = L.map("map").setView([51.505, -0.09], 13);
+let circleZoomMax = 18;
+let circleZoomMin = 13;
+let map = L.map("map", {
+  minZoom: circleZoomMin,
+  maxZoom: circleZoomMax,
+}).setView([46.5547, 15.6459], 15);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
 const canvas = document.querySelector("#myCanvas");
 const mapP = document.querySelector("#map");
 const drawButton = document.querySelector(".draw");
 const undrawButton = document.querySelector(".undraw");
 undrawButton.addEventListener("click", function () {
-  canvas.style.zIndex = -1;
-  mapP.style.zIndex = 2;
+  canvas.style.zIndex = 1;
+  mapP.style.zIndex = 1;
 });
 console.log(drawButton);
+function drawOnMap() {
+  console.log(positionOnMap.position);
+}
+let lat, lng;
+let position;
+let positionOnMap;
 drawButton.addEventListener("click", function () {
   canvas.style.zIndex = 2;
   mapP.style.zIndex = -1;
@@ -20,45 +32,87 @@ drawButton.addEventListener("click", function () {
   let ctx = canvas.getContext("2d");
   //getMouse position
 
-  let position = {
+  position = {
     x: 0,
     y: 0,
   };
 
+  positionOnMap = {
+    position: [{ positionX: 0, positionY: 0 }],
+  };
   const getMousePosition = function (e) {
     let xP = e.pageX;
     let yP = e.pageY;
     position.x = xP;
     position.y = yP;
   };
-  let color;
 
   const mouseDraw = function (e) {
     if (!isDrawing) return;
-    ctx.beginPath();
-    ctx.lineWidth = 12;
 
     ctx.lineCap = "round";
     const changeColor = document.querySelectorAll(".changeColor");
-
+    let color;
     changeColor.forEach((el) =>
       el.addEventListener("click", function (e) {
         ctx.strokeStyle = e.target.dataset.color;
+        color = e.target.dataset.color;
       })
     );
+    color = ctx.strokeStyle;
     /*
   blueColor.addEventListener("click", function (e) {
     console.log(e.target.dataset.color);
     ctx.strokeStyle = e.target.dataset.color;
   }),
   */
-    ctx.moveTo(position.x, position.y);
+    //ctx.moveTo(position.x, position.y);
     getMousePosition(e);
+    //ctx.lineTo(position.x, position.y);
+    let zoom = map.getZoom();
+    let declaredRadius;
 
-    ctx.lineTo(position.x, position.y);
+    switch (zoom) {
+      case 18:
+        desiredRadius = 20;
 
-    ctx.stroke();
+        break;
+      case 17:
+        desiredRadius = 40;
+
+        break;
+      case 16:
+        desiredRadius = 60;
+
+        break;
+      case 15:
+        desiredRadius = 80;
+      case 14:
+        desiredRadius = 100;
+
+        break;
+      case 13:
+        desiredRadius = 200;
+
+        break;
+
+      default:
+        break;
+    }
+
+    let { lat, lng } = map.layerPointToLatLng([position.x, position.y]);
+    positionOnMap.position.positionX = lat;
+    positionOnMap.position.positionY = lng;
+    L.circle([lat, lng], {
+      color: color,
+      radius: desiredRadius,
+      opacity: 0.5,
+      fillOpacity: 1,
+    }).addTo(map);
+    console.log(map.getZoom());
+    console.log(desiredRadius);
   };
+
   function startDrawing(e) {
     isDrawing = true;
     getMousePosition(e);
