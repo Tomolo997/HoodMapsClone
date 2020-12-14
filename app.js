a8dcf776dd11334512cc004de3fffe7caa74fc9a
Tomolo997 <tomazovsenjak7@gmail.com>
2020-12-14 09:17:57 +0100

    added text(unfinised) corrected the fixed the drawing

--- app.js
+++ app.js
@@ -18,47 +18,47 @@ undrawButton.addEventListener("click", function () {
   canvas.style.zIndex = 1;
   mapP.style.zIndex = 1;
 });
-console.log(drawButton);
-function drawOnMap() {
-  console.log(positionOnMap.position);
-}
+
 let lat, lng;
 let position;
 let positionOnMap;
+let ctx = canvas.getContext("2d");
 drawButton.addEventListener("click", function () {
   canvas.style.zIndex = 2;
   mapP.style.zIndex = -1;
   let isDrawing = false;
-  let ctx = canvas.getContext("2d");
+
   //getMouse position
 
   position = {
     x: 0,
     y: 0,
   };
-
+  const yea = map.getPixelBounds();
+  console.log(yea);
   positionOnMap = {
     position: [{ positionX: 0, positionY: 0 }],
   };
   const getMousePosition = function (e) {
-    let xP = e.pageX;
-    let yP = e.pageY;
+    let xP = e.clientX;
+    let yP = e.clientY;
     position.x = xP;
     position.y = yP;
   };
-
+  const changeColor = document.querySelectorAll(".changeColor");
+  let color = "red";
+  ctx.strokeStyle = "red";
+  changeColor.forEach((el) =>
+    el.addEventListener("click", function (e) {
+      ctx.strokeStyle = e.target.dataset.color;
+      color = e.target.dataset.color;
+    })
+  );
   const mouseDraw = function (e) {
     if (!isDrawing) return;
 
     ctx.lineCap = "round";
-    const changeColor = document.querySelectorAll(".changeColor");
-    let color;
-    changeColor.forEach((el) =>
-      el.addEventListener("click", function (e) {
-        ctx.strokeStyle = e.target.dataset.color;
-        color = e.target.dataset.color;
-      })
-    );
+
     color = ctx.strokeStyle;
     /*
   blueColor.addEventListener("click", function (e) {
@@ -70,7 +70,7 @@ drawButton.addEventListener("click", function () {
     getMousePosition(e);
     //ctx.lineTo(position.x, position.y);
     let zoom = map.getZoom();
-    let declaredRadius;
+    let desiredRadius;
 
     switch (zoom) {
       case 18:
@@ -100,17 +100,27 @@ drawButton.addEventListener("click", function () {
         break;
     }
 
-    let { lat, lng } = map.layerPointToLatLng([position.x, position.y]);
+    let { lat, lng } = map.containerPointToLatLng([position.x, position.y]);
     positionOnMap.position.positionX = lat;
     positionOnMap.position.positionY = lng;
-    L.circle([lat, lng], {
-      color: color,
-      radius: desiredRadius,
-      opacity: 0.5,
-      fillOpacity: 1,
-    }).addTo(map);
-    console.log(map.getZoom());
-    console.log(desiredRadius);
+
+    L.circle(
+      [positionOnMap.position.positionX, positionOnMap.position.positionY],
+      {
+        color: color,
+        radius: desiredRadius,
+        opacity: 0.5,
+        fillOpacity: 1,
+      }
+    ).addTo(map);
+
+    //console.log(position);
+    //console.log(positionOnMap);
+    let yea = map.latLngToLayerPoint([lat, lng]);
+    let xCoords = yea.x;
+    let yCoords = yea.y;
+    console.log(position);
+    console.log(positionOnMap.position);
   };
 
   function startDrawing(e) {
@@ -123,4 +133,23 @@ drawButton.addEventListener("click", function () {
   canvas.addEventListener("mousedown", startDrawing);
   canvas.addEventListener("mousemove", mouseDraw);
   canvas.addEventListener("mouseup", stopDrawing);
+});
+
+//add a text
+
+const addText = document.querySelector(".text");
+addText.addEventListener("click", function (e) {
+  map.on("click", function (e) {
+    console.log(e.latlng);
+    let latlng = [e.latlng.lat, e.latlng.lng];
+    var popup = L.popup({
+      maxWidth: 250,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+    })
+      .setContent("<input class='input'>")
+      .setLatLng(latlng)
+      .openOn(map);
+  });
 });
